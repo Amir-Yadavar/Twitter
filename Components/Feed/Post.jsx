f7a0245e4fdab617e5875870a16106f152f3464e
amir <amiresya@gmail.com>
2023-06-21 20:30:22 +0430

    add style post component of feed component

--- Components/Feed/Post.jsx
+++ Components/Feed/Post.jsx
@@ -1,9 +1,16 @@
-import { EllipsisHorizontalIcon } from "@heroicons/react/24/outline";
+import {
+  ChartBarIcon,
+  ChatBubbleBottomCenterIcon,
+  EllipsisHorizontalIcon,
+  HeartIcon,
+  ShareIcon,
+  TrashIcon,
+} from "@heroicons/react/24/outline";
 import Image from "next/image";
 
 export default function Post({ post }) {
   return (
-    <div className="flex p-3">
+    <div className="flex p-3 cursor-pointer border-b">
       <div className="">
         <Image
           src={post.imgProfile}
@@ -17,9 +24,9 @@ export default function Post({ post }) {
       <div className="flex flex-col w-full">
         <div className="flex items-center justify-between p-2">
           <div className="flex items-center">
-            <h2 className="font-bold text-gray-800 px-1">{post.name}</h2>
-            <h3 className="text-gray-600 px-1 text-sm">{post.userName}</h3>
-            <span className="text-gray-500 text-xs px-1">
+            <h2 className="font-bold text-gray-800 px-1 whitespace-nowrap">{post.name}</h2>
+            <h3 className="text-gray-600 px-1 text-sm whitespace-nowrap">{post.userName}</h3>
+            <span className="text-gray-500 text-xs px-1 whitespace-nowrap">
               {post.timestamps}
             </span>
           </div>
@@ -31,16 +38,22 @@ export default function Post({ post }) {
         <p className="p-2">{post.title}</p>
 
         <div className="p-2">
-            <Image 
+          <Image
             src={post.imgPost}
             alt="amir-yadavar"
-            className="w-full h-96 "
+            className="w-full h-96 rounded-2xl"
             width={500}
             height={385}
-            />
+          />
         </div>
 
-        <div className="flex items-center justify-around p-2"></div>
+        <div className="flex items-center justify-around p-2">
+          <ChatBubbleBottomCenterIcon className="iconHover hover:text-sky-500" />
+          <TrashIcon className="iconHover hover:text-red-500 hover:bg-red-100" />
+          <HeartIcon className="iconHover hover:text-red-500 hover:bg-red-100" />
+          <ShareIcon className="iconHover hover:text-sky-500" />
+          <ChartBarIcon className="iconHover hover:text-sky-500" />
+        </div>
       </div>
     </div>
   );
